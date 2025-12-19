4ed9f9e3f7e69c19f519f87eac52218b01a77e8d
david_mendoza <mendozadavidprogramacion2.0@gmail.com>
2025-12-19 12:39:11 -0400

    ya se puede actualizar ek usuario

--- NetCoreBlazor-main-remake.Client/Services/UsuarioService.cs
+++ NetCoreBlazor-main-remake.Client/Services/UsuarioService.cs
@@ -31,7 +31,7 @@ namespace NetCoreBlazor_main_remake.Client.Services
 
         public async Task<UsuarioDTO?> GetUsuarioAsync(int id)
         {
-            if (id <= 0) return null; // evita llamadas con id=0
+            if (id <= 0) return null;
 
             await AddAuthorizationHeader();
             var response = await _http.GetAsync($"api/Usuario/{id}");
